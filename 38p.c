9605f9c8a1fe4b7e4c74dfec8d1244185aefd3e8
prajapatiyug12345-create <prajapatiyug.12345@gmail.com>
2025-09-11 11:21:37 +0530

    Create 38p.c

---
+++ 38p.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n, sum=0;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    for(int i=1; i<=2*n-1; i+=2) {
+        sum += i;
+    }
+    printf("Sum = %d\n", sum);
+    return 0;
+}
